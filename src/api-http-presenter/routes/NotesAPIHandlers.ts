fd56d88736471503f501af9d96461c0e68653ca7
zero <theloostt@live.com>
2022-10-17 23:59:03 -0300

    feat: implement create folders files routes

--- src/api-http-presenter/routes/NotesAPIHandlers.ts
+++ src/api-http-presenter/routes/NotesAPIHandlers.ts
@@ -58,132 +58,48 @@ export default class NotesAPIHandlers {
     })
   }
 
-  // get'/notes'
-  static async getCategories(req: any, res: any) {
-    const categories = await fs.readdir(notesDir);
-  
-    res.json(categories);
+  // post'/notes/folder'
+  static async createFolder(req: any, res: any) {
+    const {directory, name} = req.body;
+    const finalDir = path.join(notesDir, ...directory, name);
+    await fs.mkdir(finalDir);
+
+    res.json({
+      status: 'ok',
+      err: '',
+      body:{}
+    })
   }
 
-  //get'/notes/:category/content', 
-  static async getNotebooks(req, res) {
-    const { category } = req.params;
-    const categoryDir = path.join(notesDir, category);
+  // post'/notes/note'
+  static async createNote(req: any, res: any) {
+    let {directory, name} = req.body;
+    if (!name.match(/.+\.md$/))
+      name += '.md'
+    const finalDir = path.join(notesDir, ...directory, name)
+    await fs.writeFile(finalDir, '', 'utf-8');
 
-    const notebooks = await fs.readdir(categoryDir);
+    res.json({
+      status: 'ok',
+      err: '',
+      body: {}
+    });
+  }
+
+  // post'/notes/save-note
+  static async saveNote(req: any, res:any) {
+    const { directory, content } = req.body;
     
-    res.json(notebooks);
-  }
+    const finalDir = path.join(notesDir, ...directory);
 
-  //get'/notes/:category/:notebook/content'
-  static async getSections(req, res) {
-    const { category, notebook } = req.params;
-  
-    const notebookDir = path.join(notesDir, category, notebook)
-  
-    const sections = await fs.readdir(notebookDir);
-  
-    res.json(sections);
-  }
+    await fs.writeFile(directory, content, 'utf-8');
 
-  //post'/notes/:category/:notebook/section/content'
-  static async getSectionSections(req, res) {
-    const { category, notebook } = req.params;
-    const { section } = req.body;
-  
-    const sectionDir = path.join(notesDir, category, notebook, section);
-  
-    const pages = await fs.readdir(sectionDir);
-    
-    res.json(pages);
-  }
+    res.json({
+      status: 'ok',
+      err: '',
+      body: {
 
-  //post'/notes/:category/:notebook/section/page'
-  static async getPage(req, res) {
-    const { category, notebook } = req.params;
-    const { section, page } = req.body;
-    console.log(section)
-    const pageDir = path.join(notesDir, category, notebook, section, page);
-  
-    const pageContent = await fs.readFile(pageDir, 'utf-8');
-  
-    res.json(pageContent)
-  }
-
-  //post'/notes/:category/new-notebook'
-  static async createNewNotebook(req, res) {
-    const { category } = req.params;
-    const { name } = req.body;
-  
-    const newNotebookDir = path.join(notesDir, category, name);
-  
-    await fs.mkdir(newNotebookDir, { recursive: true });
-  
-    res.json({ok: true});
-  }
-
-  //post'/notes/new-section'
-  static async createNewSection(req, res) {
-    const { name, pathDir } = req.body;
-  
-    const newSectionDir = path.join(notesDir, pathDir, name);
-    console.log(newSectionDir)
-    await fs.mkdir(newSectionDir, { recursive: true });
-  
-    res.json({ok: true});
-  }
-
-  //post'/notes/new-page'
-  static async createNewPage(req, res) {
-    let { name, pathDir } = req.body;
-  
-    if (!name.match(/\.md$/)) name = name+'.md'
-    
-    const newPageDir = path.join(notesDir, pathDir, name);
-  
-    await fs.writeFile(newPageDir, '', 'utf-8');
-  
-    res.json({ok: true});
-  }
-
-  //put'/notes/:category/:notebook/section/page/save'
-  static async savePage(req, res) {
-    const { category, notebook } = req.params;
-    const { content, section, page } = req.body;
-  
-    const newPageDir = path.join(notesDir, category, notebook, section, page);
-  
-    await fs.writeFile(newPageDir, content, 'utf-8');
-  
-    res.json({ok: true});
-  }
-
-  //delete'/notes/:category/:notebook/notebook/delete'
-  static async deleteNotebook(req, res) {
-    const { category, notebook } = req.params;
-  
-    fs.rmdir(path.join(notesDir,category, notebook), {recursive:true})
-  
-    res.json({ok:true})
-  }
-  
-  //delete'/notes/:category/:notebook/section/delete'
-  static async deleteSection(req, res) {
-    const { section } = req.body
-    
-    const { category, notebook } = req.params;
-    fs.rmdir(path.join(notesDir,category, notebook, section), {recursive:true})
-  
-    res.json({ok:true})
-  }
-  
-  //delete'/notes/:category/:notebook/page/delete'
-  static async deletePage(req, res) {
-    const { category, notebook } = req.params;
-    const { section, page } = req.body
-  
-    fs.rm(path.join(notesDir,category, notebook, section, page))
-  
-    res.json({ok:true})
+      }
+    })
   }
 }
--- src/api-http-presenter/routes/NotesAPIRoutes.ts
+++ src/api-http-presenter/routes/NotesAPIRoutes.ts
@@ -8,18 +8,18 @@ export default [
     method: 'get', path: '/notes/note/*',
     handler: NotesAPIHandlers.getNote
   },
-  // {
-  //   method: 'post', path: '/notes/folder',
-  //   handler: NotesAPIHandlers.createFolder
-  // },
-  // {
-  //   method: 'post', path: '/notes/note',
-  //   handler: NotesAPIHandlers.createNote
-  // },
-  // {
-  //   method: 'post', path: '/notes/save-note/*/*.md',
-  //   handler: NotesAPIHandlers.saveNote
-  // },
+  {
+    method: 'post', path: '/notes/folder',
+    handler: NotesAPIHandlers.createFolder
+  },
+  {
+    method: 'post', path: '/notes/note',
+    handler: NotesAPIHandlers.createNote
+  },
+  {
+    method: 'post', path: '/notes/save-note/*/*.md',
+    handler: NotesAPIHandlers.saveNote
+  },
   // {
   //   method: 'delete', path: '/notes/note/*/*.md',
   //   handler: NotesAPIHandlers.deleteFolder
