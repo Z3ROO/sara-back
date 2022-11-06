2c1ccfd4497e236cb3d4b4c3e82dcf5178b37b40
z3roo <theloostt@live.com>
2022-11-05 23:32:26 -0300

    fix: validate old tests

--- src/features/Records.ts
+++ src/features/Records.ts
@@ -1,11 +1,12 @@
 import RecordsRepo from "../repositories/RecordsRepo";
 import { INewRecord, IRecords } from "./interfaces/interfaces";
 import { nextAcceptanceLevel } from "./Feats";
+import { isObjectId } from "../infra/database/mongodb";
+import { BadRequest } from "../util/errors/HttpStatusCode";
 
 export class Records {
   static async getAllRecords() {
     const records = await RecordsRepo.findAllRecords();
-
     return records
   }
 
@@ -14,14 +15,12 @@ export class Records {
       categories = [categories];
 
     const records = await RecordsRepo.findRecordsByCategory(categories);
-
-    return records
+    return records;
   }
 
   static async getRecordsByQuestline(questline: string) {
     const records = await RecordsRepo.findRecordsByQuestline(questline);
-
-    return records
+    return records;
   }
 
   static async getRecordsWithHistoryInOneDay(date: Date) {
@@ -29,25 +28,60 @@ export class Records {
     const end = new Date(date.setHours(23,59,59));
     
     const records = await RecordsRepo.findEveryRecordsHistoryInDateRange({begin, end});
-
     return records;
   }
 
-  static async updateRecordLevel(identifier: string, direction: -1|0|1) {
+  static async updateRecordLevel(record_id: string, direction: -1|0|1) {
+    if (!isObjectId(record_id))
+      throw new BadRequest('Invalid record_id');
+
     if (direction < -1 || direction > 1)
       throw new Error('Direction must be -1, 0 or 1');
 
-    await RecordsRepo.updateRecordLevel({_id: identifier}, direction);
+    await RecordsRepo.updateRecordLevel(record_id, direction);
   }
 
   static async createNewRecord(properties: INewRecord) {    
-    await RecordsRepo.insertOneRecord(properties);
+    const {
+      questline_id,
+      skill_id,
+      title,
+      description,
+      metric,
+      status,
+      categories
+    } = properties;
+
+    if (!isObjectId(questline_id))
+      throw new BadRequest('Invalid questline_id');
+
+    await RecordsRepo.insertOneRecord({
+      questline_id: questline_id ? questline_id : null,
+      skill_id: skill_id ? skill_id : null,
+      title,
+      description,
+      acceptance: {
+        stage: 'created',
+        date: [new Date()]
+      },
+      metric,
+      status: {
+        waitTime: status.waitTime,
+        stageAmount: status.stageAmount,
+        stage: null,
+        last_commitment: null
+      },
+      categories: categories ? categories : [],
+      level: 0,
+      history: [],
+      xp: null
+    });
   }
 
-  static async proceedFeatAcceptanceLevel(identifier: string) {
-    const feat = await RecordsRepo.findOneRecord({_id: identifier});
+  static async proceedFeatAcceptanceLevel(record_id: string) {
+    const feat = await RecordsRepo.findOneRecord(record_id);
     const stage = nextAcceptanceLevel(feat);
 
-    await RecordsRepo.proceedAcceptanceLevel({_id: identifier}, stage);
+    await RecordsRepo.updateAcceptanceLevel(record_id, stage);
   }
 }
